5f56fd30ff1c03325aaa881e2b79b1ffde3c4ee1
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-02-18 13:58:37 +0100

    MySQL connection

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import os
+import requests
+import json
+import mysql.connector
+import dotenv
+
+dotenv.load_dotenv()
+
+
+def connecter_base_de_donnees() -> mysql.connector.MySQLConnection | None:
+    """
+    Connecte à la base de données.
+    """
+    try:
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+        )
+    except mysql.connector.Error as err:
+        # TODO: Envoyer notification TEAMS
+        print(f"Erreur de connexion à la base de données : {err}")
+        return None
+
+
+def deconnecter_base_de_donnees(
+    connexion: mysql.connector.MySQLConnection,
+) -> None:
+    """
+    Déconnecte de la base de données.
+    """
+    connexion.close()
 
 
 def calculer_taille_dossier(chemin_dossier: str) -> int:
@@ -32,3 +65,5 @@ def lister_tous_les_dossier(chemin_racine: str) -> list[str]:
 
 liste_des_dossiers = lister_tous_les_dossier("D:\\Exemple\\")
 print(liste_des_dossiers)
+
+print(connecter_base_de_donnees())
